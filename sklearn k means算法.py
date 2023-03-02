4e8fa30d8ffce36d1986608426f1cd256fedb19c
OPUS-Lightphenexx <124802724+OPUS-Lightphenexx@users.noreply.github.com>
2023-03-02 23:12:48 +0800

    Add files via upload

---
+++ sklearn k means算法.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+#原始数据
+train_x_test = np.array([[90,100],[88,90],[85,95],[10,20],[30,40],[50,30],[80,56],[67,80],[90,90]])
+
+plot_x = train_x_test[:,0]
+plot_y = train_x_test[:,1]
+
+cluster2 = KMeans(n_clusters=2).fit_predict(train_x_test)
+cluster3 = KMeans(n_clusters=3).fit_predict(train_x_test)
+cluster4 = KMeans(n_clusters=4).fit_predict(train_x_test)
+cluster5 = KMeans(n_clusters=6).fit_predict(train_x_test)
+
+#SSE 手寸方法确定k值
+SSE_data = []
+for i in range(2,10):
+    kmeans = KMeans(n_clusters=i)
+    result_list = kmeans.fit_predict(train_x_test)
+    SSE = kmeans.inertia_
+    SSE_data.append(SSE)
+
+#画图
+plt.subplot(221)
+plt.scatter(plot_x,plot_y,c=cluster2)
+plt.subplot(222)
+plt.scatter(plot_x,plot_y,c=cluster3)
+plt.subplot(223)
+plt.scatter(plot_x,plot_y,c=cluster5)
+plt.subplot(224)
+plt.plot(range(2,10),SSE_data,label='SSE')
+plt.title('SSE')
+
+plt.show()
