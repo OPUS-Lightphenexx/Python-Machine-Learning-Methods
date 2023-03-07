95bf1870539de2c56e8bd4de9da114a4687bb485
Lightphenexx <lightphenexx@outlook.com>
2023-03-07 23:17:48 +0800

    Add files via upload

---
+++ 逻辑回归决策边界(Logistic Regression Decision Boundary).py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+Datax = np.array([[3,2],[4,5],[6,5],[7,3],[8,5]])
+Datay = np.array([0,0,0,1,1])
+
+x_test = np.array([[10,5]])
+
+model = LogisticRegression()
+model.fit(Datax, Datay)
+
+#实行分类
+sort = np.argsort(np.where(Datay==0)).flatten()
+Data_is_0 = Datax[0:len(sort)]
+print(Data_is_0)
+
+x_is_0 = Data_is_0[:,0]
+y_is_0 = Data_is_0[:,1]
+plt.scatter(x_is_0,y_is_0,color='r')
+
+Data_is_1 = Datax[len(sort):]
+x_is_1 = Data_is_1[:,0]
+y_is_1 = Data_is_1[:,1]
+
+plt.scatter(x_is_1,y_is_1,color='b')
+print(model.coef_)
+print(model.intercept_)
+#画决策边界
+
+x1_plot = np.linspace(0, 10,100)
+x2_plot = (-model.coef_[0][0] * x1_plot - model.intercept_[0]) / model.coef_[0][1]
+plt.plot(x1_plot,x2_plot)
+plt.show()
+
