672175d134208b3018e202f50260b73bce882f78
Lightphenexx <lightphenexx@outlook.com>
2023-03-27 22:55:31 +0800

    Update 决策树分类预测.py

--- 决策树分类预测.py
+++ 决策树分类预测.py
@@ -40,7 +40,30 @@ ax3.scatter(x_data,y_data,z_data,c=y_train_label)
 ax3.scatter(x_test_3D[:,0],x_test_3D[:,1],x_test_3D[:,2],color='red')
 plt.show()
 
+#高纬度特征决策树
+d_x_test = np.array([[1,1,1,1,1,1],
+                     [2,1,2,1,1,1],
+                     [2,1,1,1,1,1],
+                     [1,1,2,1,1,1],
+                     [3,1,1,1,1,1],
+                     [1,2,1,1,2,2],
+                     [4,2,1,1,2,2],
+                     [4,2,1,1,2,1],
+                     [4,2,2,1,2,1],
+                     [1,3,3,3,3,2],
+                     [3,3,3,2,3,1],
+                     [3,1,2,2,3,2],
+                     [1,2,2,3,1,1],
+                     [3,2,2,3,1,1],
+                     [2,2,1,1,2,2],
+                     [3,1,1,2,3,1],
+                     [1,1,2,2,2,1]])
+d_y_test_label = np.array([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0])
+plt.scatter(d_x_test[:,0],d_x_test[:,1],c=d_y_test_label)
 
+tree_clf.fit(d_x_test,d_y_test_label)
 
-
+x_test_111 = np.array([[2,4,3,2,4,5]])
+new_predict = tree_clf.predict(x_test_111)
+print(new_predict)
 
