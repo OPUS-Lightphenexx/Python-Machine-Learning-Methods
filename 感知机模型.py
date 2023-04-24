f1a107a3da97c976d27ffbe4423afc43f72c32c4
OPUS-Lightphenexx <lightphenexx@outlook.com>
2023-04-24 21:07:29 +0800

    Perceptron Using Sklearn

--- 感知机模型.py
+++ 感知机模型.py
@@ -1,26 +1,54 @@
 import numpy as np
+from sklearn.linear_model import Perceptron
+import pandas as pd
+import matplotlib.pyplot as plt
 
-x_i = np.array([[1,2],[2,2],[4,5]])
-r_i = np.array([[0],[1],[0]])
-
-w_i = 2
-print(w_i)
-h_number = 2
-print(h_number)
-h = np.array([[h_number,h_number]])
-
-Multiply_x_i_w_i = w_i*x_i
-list_test = []
-print(Multiply_x_i_w_i)
-print(h)
+#M-P模型
+#一维向量模型
+x_i = np.array([1,3,5,8]).reshape(-1,1)
+r_i = np.array([0,0,1,1])
+w_i = np.array([1,1,1,1])
+r = 0
 
 
-times = 10
-for i in Multiply_x_i_w_i:
-    if Multiply_x_i_w_i.any()>h.any():
-        list_test.append(1)
+h = 13
+def func(x_i,w_i,h):
+    y = np.dot(x_i,w_i)
+    if y>h:
+        return 1
     else:
-        list_test.append(0)
+        return 0
 
 
-print(list_test)
+#end一下
+
+#用sklearn节省调参过程
+#利用pandas创建表格
+Dict = {'x1':[1,4,3,2,5,6,7,7],'x2':[4,8,6,3,5,6,13],'label':[0,0,0,1,1,1,1]}
+Data_table = pd.DataFrame(Dict)
+print(Data_table)
+perceptron = Perceptron(fit_intercept=True, max_iter=1000, shuffle=True)
+x_ip = Data_table[['x1','x2']]
+print(x_ip)
+r_ip = Data_table['label']
+
+perceptron.fit(x_ip,r_ip)
+w_ip = perceptron.coef_[0]
+print(perceptron.coef_[0])
+b = perceptron.intercept_
+print(b)
+
+plt.scatter(Data_table['x1'],Data_table['x2'],c=Data_table['label'])
+xs = np.linspace(0,7,100)
+plt.plot(xs,(w_ip[0]*xs+b)/(-w_ip[1]))
+plt.show()
+
+
+
+
+        
+
+
+
+
+
